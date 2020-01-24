f8a08601b01eae1a98f7643c12baf7249b4fc32a
Vidya <kvv.vidya@gmail.com>
2020-01-23 22:01:21 -0800

    msg

--- Leetcode/Leetcode_905.py
+++ Leetcode/Leetcode_905.py
@@ -10,21 +10,25 @@ The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
 from typing import List
 class Solution:
     def sortArrayByParity(self, A: List[int]) -> List[int]:
-        for index in range(int((len(A)+1)/2)):
-            if ((A[index] % 2) == 1) and ((A[~index] % 2) == 0):
-                temp = A[index];
-                A[index] = A[~index]
-                A[~index] = temp
-            elif ((A[index] % 2) == 1) and ((A[~index] % 2) == 1):
-                temp = A.pop(index)
-                A.append(temp)
-            elif ((A[index] % 2) == 0) and ((A[~index] % 2) == 0):
-                temp = A.pop(~index)
-                A.insert(index+1,temp)
+        my_list = [0] * len(A)
+        #print(my_list)
+        start_index = 0
+        end_index = len(A)-1
+        for element in A:
+            if element % 2 == 0 :
+                my_list.remove(my_list[start_index])
+                my_list.insert(start_index, element)
+                start_index += 1
             else:
-                continue
-        return A
-# maintain 2 index start and end
+                my_list.remove(my_list[end_index])
+                my_list.insert(end_index, element)
+                end_index -= 1
+
+        return my_list
+"""
+return ([x for x in A if x % 2 == 0] +
+                [x for x in A if x % 2 == 1])
+"""
 if __name__ == "__main__":
     my_solution = Solution()
     print(my_solution.sortArrayByParity([6,3,1,7,2,4]))
